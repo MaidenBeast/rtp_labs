eea291e2b24b7240df2544a8b809e07b95e6c4fc
fel.sallustio <fel.sallustio@61381455-f0db-496b-9492-d7ffbf3e31ee>
2015-04-01 14:12:12 +0000

    First commit of RTP lab 1

---
+++ rtp_labs/lab1/lab-1e/PENTIUMdiab/lab-1e_partialImage/Debug/Objects/lab-1e/clocks.d
@@ -0,0 +1,216 @@
+lab-1e_partialImage/Debug/Objects/lab-1e/clocks.o: \
+  C:/windriver/workspace/lab-1e/clocks.c \
+  C:/windriver/vxworks-6.9/target/h/stdio.h \
+  C:/windriver/vxworks-6.9/target/h/types/vxANSI.h \
+  C:/windriver/vxworks-6.9/target/h/stdint.h \
+  C:/windriver/vxworks-6.9/target/h/vsbConfig.h \
+  C:/windriver/vxworks-6.9/target/lib/h/config/vsbConfig.h \
+  C:/windriver/vxworks-6.9/target/lib/h/config/autoconf.h \
+  C:/windriver/vxworks-6.9/target/h/types/vxCpu.h \
+  C:/windriver/vxworks-6.9/target/h/types/vxArch.h \
+  C:/windriver/vxworks-6.9/target/h/arch/i86/archI86.h \
+  C:/windriver/vxworks-6.9/target/h/arch/i86/archI86Common.h \
+  C:/windriver/vxworks-6.9/target/h/types/vxParams.h \
+  C:/windriver/vxworks-6.9/target/h/types/vxTypesBase.h \
+  C:/windriver/vxworks-6.9/target/usr/h/stddef.h \
+  C:/windriver/vxworks-6.9/target/usr/h/yvals.h \
+  C:/windriver/vxworks-6.9/target/h/errno.h \
+  C:/windriver/vxworks-6.9/target/h/private/handleLibP.h \
+  C:/windriver/vxworks-6.9/target/h/vxWorks.h \
+  C:/windriver/vxworks-6.9/target/h/vxWorksCommon.h \
+  C:/windriver/vxworks-6.9/target/h/types/vxTypes.h \
+  C:/windriver/vxworks-6.9/target/h/base/b_ssize_t.h \
+  C:/windriver/vxworks-6.9/target/h/base/b_cpuset_t.h \
+  C:/windriver/vxworks-6.9/target/h/base/b_phys_cpuset_t.h \
+  C:/windriver/vxworks-6.9/target/h/base/b_atomic_t.h \
+  C:/windriver/vxworks-6.9/target/h/base/b_time_t.h \
+  C:/windriver/vxworks-6.9/target/h/base/b_dev_t.h \
+  C:/windriver/vxworks-6.9/target/h/base/b_gid_t.h \
+  C:/windriver/vxworks-6.9/target/h/base/b_ino_t.h \
+  C:/windriver/vxworks-6.9/target/h/base/b_mode_t.h \
+  C:/windriver/vxworks-6.9/target/h/base/b_nlink_t.h \
+  C:/windriver/vxworks-6.9/target/h/base/b_off_t.h \
+  C:/windriver/vxworks-6.9/target/h/base/b_pid_t.h \
+  C:/windriver/vxworks-6.9/target/h/base/b_uid_t.h \
+  C:/windriver/vxworks-6.9/target/h/base/b_blksize_t.h \
+  C:/windriver/vxworks-6.9/target/h/base/b_blkcnt_t.h \
+  C:/windriver/vxworks-6.9/target/h/types/vxTypesOld.h \
+  C:/windriver/vxworks-6.9/target/h/base/e_vxTypesOld.h \
+  C:/windriver/vxworks-6.9/target/h/sys/types.h \
+  C:/windriver/vxworks-6.9/target/h/base/b_pthread_attr_t.h \
+  C:/windriver/vxworks-6.9/target/h/base/b_size_t.h \
+  C:/windriver/vxworks-6.9/target/h/base/b_struct__Sched_param.h \
+  C:/windriver/vxworks-6.9/target/h/base/b_struct__Timespec.h \
+  C:/windriver/vxworks-6.9/target/h/base/b_clock_t.h \
+  C:/windriver/vxworks-6.9/target/h/base/b_key_t.h \
+  C:/windriver/vxworks-6.9/target/h/base/b_timer_t.h \
+  C:/windriver/vxworks-6.9/target/h/base/b_ptrdiff_t.h \
+  C:/windriver/vxworks-6.9/target/h/base/b_BOOL.h \
+  C:/windriver/vxworks-6.9/target/h/types/vxWind.h \
+  C:/windriver/vxworks-6.9/target/h/types/vxWindCommon.h \
+  C:/windriver/vxworks-6.9/target/h/tool/diab/toolMacros.h \
+  C:/windriver/vxworks-6.9/target/h/tool/common/private/toolMacrosP.h \
+  C:/windriver/vxworks-6.9/target/h/version.h \
+  C:/windriver/diab/5.9.1.0/include/diab/stdarg.h \
+  C:/windriver/diab/5.9.1.0/include/diab/va_list.h \
+  C:/windriver/vxworks-6.9/target/usr/h/sys/time.h \
+  C:/windriver/vxworks-6.9/target/usr/h/base/b_suseconds_t.h \
+  C:/windriver/vxworks-6.9/target/usr/h/base/b_struct_timeval.h \
+  C:/windriver/vxworks-6.9/target/usr/h/base/b_struct_itimerval.h \
+  C:/windriver/vxworks-6.9/target/usr/h/sys/select.h \
+  C:/windriver/vxworks-6.9/target/h/signal.h \
+  C:/windriver/vxworks-6.9/target/h/sigevent.h \
+  C:/windriver/vxworks-6.9/target/h/sigeventCommon.h \
+  C:/windriver/vxworks-6.9/target/h/base/b_union_sigval.h \
+  C:/windriver/vxworks-6.9/target/h/time.h \
+  C:/windriver/vxworks-6.9/target/h/objLib.h \
+  C:/windriver/vxworks-6.9/target/h/objLibCommon.h \
+  C:/windriver/vxworks-6.9/target/h/vwModNum.h \
+  C:/windriver/vxworks-6.9/target/h/limits.h \
+  C:/windriver/vxworks-6.9/target/h/tool/diab/limits.h \
+  C:/windriver/vxworks-6.9/target/h/base/b_struct_timespec.h \
+  C:/windriver/vxworks-6.9/target/h/unistd.h \
+  C:/windriver/vxworks-6.9/target/h/math.h
+
+C:/windriver/vxworks-6.9/target/h/stdio.h:
+
+C:/windriver/vxworks-6.9/target/h/types/vxANSI.h:
+
+C:/windriver/vxworks-6.9/target/h/stdint.h:
+
+C:/windriver/vxworks-6.9/target/h/vsbConfig.h:
+
+C:/windriver/vxworks-6.9/target/lib/h/config/vsbConfig.h:
+
+C:/windriver/vxworks-6.9/target/lib/h/config/autoconf.h:
+
+C:/windriver/vxworks-6.9/target/h/types/vxCpu.h:
+
+C:/windriver/vxworks-6.9/target/h/types/vxArch.h:
+
+C:/windriver/vxworks-6.9/target/h/arch/i86/archI86.h:
+
+C:/windriver/vxworks-6.9/target/h/arch/i86/archI86Common.h:
+
+C:/windriver/vxworks-6.9/target/h/types/vxParams.h:
+
+C:/windriver/vxworks-6.9/target/h/types/vxTypesBase.h:
+
+C:/windriver/vxworks-6.9/target/usr/h/stddef.h:
+
+C:/windriver/vxworks-6.9/target/usr/h/yvals.h:
+
+C:/windriver/vxworks-6.9/target/h/errno.h:
+
+C:/windriver/vxworks-6.9/target/h/private/handleLibP.h:
+
+C:/windriver/vxworks-6.9/target/h/vxWorks.h:
+
+C:/windriver/vxworks-6.9/target/h/vxWorksCommon.h:
+
+C:/windriver/vxworks-6.9/target/h/types/vxTypes.h:
+
+C:/windriver/vxworks-6.9/target/h/base/b_ssize_t.h:
+
+C:/windriver/vxworks-6.9/target/h/base/b_cpuset_t.h:
+
+C:/windriver/vxworks-6.9/target/h/base/b_phys_cpuset_t.h:
+
+C:/windriver/vxworks-6.9/target/h/base/b_atomic_t.h:
+
+C:/windriver/vxworks-6.9/target/h/base/b_time_t.h:
+
+C:/windriver/vxworks-6.9/target/h/base/b_dev_t.h:
+
+C:/windriver/vxworks-6.9/target/h/base/b_gid_t.h:
+
+C:/windriver/vxworks-6.9/target/h/base/b_ino_t.h:
+
+C:/windriver/vxworks-6.9/target/h/base/b_mode_t.h:
+
+C:/windriver/vxworks-6.9/target/h/base/b_nlink_t.h:
+
+C:/windriver/vxworks-6.9/target/h/base/b_off_t.h:
+
+C:/windriver/vxworks-6.9/target/h/base/b_pid_t.h:
+
+C:/windriver/vxworks-6.9/target/h/base/b_uid_t.h:
+
+C:/windriver/vxworks-6.9/target/h/base/b_blksize_t.h:
+
+C:/windriver/vxworks-6.9/target/h/base/b_blkcnt_t.h:
+
+C:/windriver/vxworks-6.9/target/h/types/vxTypesOld.h:
+
+C:/windriver/vxworks-6.9/target/h/base/e_vxTypesOld.h:
+
+C:/windriver/vxworks-6.9/target/h/sys/types.h:
+
+C:/windriver/vxworks-6.9/target/h/base/b_pthread_attr_t.h:
+
+C:/windriver/vxworks-6.9/target/h/base/b_size_t.h:
+
+C:/windriver/vxworks-6.9/target/h/base/b_struct__Sched_param.h:
+
+C:/windriver/vxworks-6.9/target/h/base/b_struct__Timespec.h:
+
+C:/windriver/vxworks-6.9/target/h/base/b_clock_t.h:
+
+C:/windriver/vxworks-6.9/target/h/base/b_key_t.h:
+
+C:/windriver/vxworks-6.9/target/h/base/b_timer_t.h:
+
+C:/windriver/vxworks-6.9/target/h/base/b_ptrdiff_t.h:
+
+C:/windriver/vxworks-6.9/target/h/base/b_BOOL.h:
+
+C:/windriver/vxworks-6.9/target/h/types/vxWind.h:
+
+C:/windriver/vxworks-6.9/target/h/types/vxWindCommon.h:
+
+C:/windriver/vxworks-6.9/target/h/tool/diab/toolMacros.h:
+
+C:/windriver/vxworks-6.9/target/h/tool/common/private/toolMacrosP.h:
+
+C:/windriver/vxworks-6.9/target/h/version.h:
+
+C:/windriver/diab/5.9.1.0/include/diab/stdarg.h:
+
+C:/windriver/diab/5.9.1.0/include/diab/va_list.h:
+
+C:/windriver/vxworks-6.9/target/usr/h/sys/time.h:
+
+C:/windriver/vxworks-6.9/target/usr/h/base/b_suseconds_t.h:
+
+C:/windriver/vxworks-6.9/target/usr/h/base/b_struct_timeval.h:
+
+C:/windriver/vxworks-6.9/target/usr/h/base/b_struct_itimerval.h:
+
+C:/windriver/vxworks-6.9/target/usr/h/sys/select.h:
+
+C:/windriver/vxworks-6.9/target/h/signal.h:
+
+C:/windriver/vxworks-6.9/target/h/sigevent.h:
+
+C:/windriver/vxworks-6.9/target/h/sigeventCommon.h:
+
+C:/windriver/vxworks-6.9/target/h/base/b_union_sigval.h:
+
+C:/windriver/vxworks-6.9/target/h/time.h:
+
+C:/windriver/vxworks-6.9/target/h/objLib.h:
+
+C:/windriver/vxworks-6.9/target/h/objLibCommon.h:
+
+C:/windriver/vxworks-6.9/target/h/vwModNum.h:
+
+C:/windriver/vxworks-6.9/target/h/limits.h:
+
+C:/windriver/vxworks-6.9/target/h/tool/diab/limits.h:
+
+C:/windriver/vxworks-6.9/target/h/base/b_struct_timespec.h:
+
+C:/windriver/vxworks-6.9/target/h/unistd.h:
+
+C:/windriver/vxworks-6.9/target/h/math.h:
+
---
+++ rtp_labs/lab1/lab-1e/clocks.c
@@ -0,0 +1,54 @@
+/* clocks.c
+ * Utför några tester på hur klockan fungerar med olika hastigheter 
+ */
+
+#include <stdio.h>
+#include <sys/time.h>
+#include <sys/select.h>
+#include <unistd.h>
+#include "math.h"
+
+double timenow() {
+  struct timespec tv;
+  clock_gettime(CLOCK_REALTIME,&tv);
+  return tv.tv_sec + tv.tv_nsec * 1e-9;
+}
+void delay(double dt) {
+  struct timespec tv;
+  tv.tv_sec = (int) dt;
+  tv.tv_nsec = fmod(dt,1.0) * 1000000000;
+  nanosleep(&tv,NULL);
+}
+
+void getres() {
+  struct timespec tv;
+  clock_getres(CLOCK_REALTIME,&tv);
+  printf("Clock resolution is: %.6f",tv.tv_sec+1e-9*tv.tv_nsec);
+}
+
+void clockTest() {
+  double startTime = timenow();
+  double nextTime = startTime;
+  double previousTime = -1.0; //negative time, i.e. "there is no previous time"
+  double deltaTime;
+  char *syncResultStr;
+  
+  while(1) {
+	if (previousTime == -1.0) { //if there is no previous time
+		printf("%3.6f\n",timenow() - startTime); //print only the actual relative time
+	} else {
+		//output string check of 7Hz clock runtime 
+		syncResultStr = (deltaTime == 1/7.0) ? "Sync to 7Hz" : "Not sync to 7Hz";
+		
+		// print relative time, delta time, and if the clock is "sync" to 7Hz
+		printf("%3.6f\t %3.6f\t %s\n",timenow() - startTime, deltaTime, syncResultStr);
+	}
+	printf("Clock frequency: %d\n", sysClkRateGet());
+	
+    previousTime = timenow() - startTime;
+    
+    nextTime += 1/7.0;
+    delay(nextTime - timenow());
+    deltaTime = timenow() - startTime - previousTime;
+  }
+}
