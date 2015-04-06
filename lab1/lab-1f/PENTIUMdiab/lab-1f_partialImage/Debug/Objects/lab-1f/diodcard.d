efc076695710a30392628a74eac6c8f2e1cf3e6d
fel.sallustio <fel.sallustio@61381455-f0db-496b-9492-d7ffbf3e31ee>
2015-04-06 10:37:47 +0000

    Done until point 4 of exercise F of rtp lab1

---
+++ lab1/lab-1f/PENTIUMdiab/lab-1f_partialImage/Debug/Objects/lab-1f/diodcard.d
@@ -0,0 +1,6 @@
+lab-1f_partialImage/Debug/Objects/lab-1f/diodcard.o: \
+  C:/windriver/workspace/lab-1f/diodcard.c \
+  C:/windriver/workspace/lab-1f/delayLib.h
+
+C:/windriver/workspace/lab-1f/delayLib.h:
+
