5bc53ba0bcdbd96cfae7db96969cd600e8117b5a
fel.sallustio <fel.sallustio@61381455-f0db-496b-9492-d7ffbf3e31ee>
2015-05-14 11:43:19 +0000

    Completed implementation of config mode.

--- rtp_labs/lab3/lab-3/lab3.c
+++ rtp_labs/lab3/lab-3/lab3.c
@@ -15,10 +15,10 @@ void stop() {
 
 	if (motor1_PID!=0) taskDelete(motor1_PID);
 	if (motor1_PID!=0) taskDelete(motor2_PID);
-	
+
 	if (lamp_PID!=0) taskDelete(lamp_PID);
 	if (fan_PID!=0) taskDelete(fan_PID);
-	
+
 	if (lab3_PID!=0) taskDelete(lab3_PID);
 }
 
@@ -30,55 +30,70 @@ void changeRunningMode(running_mode_t mode) {
 void lab3() {
 	sysClkRateSet(1000);
 	kernelTimeSlice(10); //enable round-robin scheduling process
-	
+
 	input = 13; //D (entering into calibrarion mode)
 	running_mode = CONFIG;
 	changeFanMode(FAN_OFF);
 	changeLampMode(LAMP_CONFIG);
-	
+
 	lamp_state = 0;
 	fan_state = 0;
 
 	lamp_PID = taskSpawn("lamp", 200, 0, 1000, lamp);
 	fan_PID = taskSpawn("fan", 201, 0, 1000, fan);
-	
+
 	sysOutByte(0x184, 0xFF); //taking control of fan and lamp ports
-	
+
 	while (1) {
-	
+
 		switch (running_mode) {
-			case CONFIG:
+		case CONFIG:
 
-				do { //reads from keyboard matrix until the user digit something on the keyboard
-					input = readKeyboard();
-					//taskDelay(10);
-				} while (input == -1);
+			do { //reads from keyboard matrix until the user digit something on the keyboard
+				input = readKeyboard();
+				//taskDelay(10);
+			} while (input == -1);
 
-				switch (input) { //	3. The following 3 operator commands can be given:
-					case 0:
-						//End configuration mode and go to run-mode.
-						changeRunningMode(RUN);
-						changeFanMode(FAN_FIFTY_PERCENT); //because the machine are not running at the beginning
-						changeLampMode(MACHINE_NOT_WORKING);
+			switch (input) { //	3. The following 3 operator commands can be given:
+			case 0:
+				//End configuration mode and go to run-mode.
+				changeRunningMode(RUN);
+				changeFanMode(FAN_FIFTY_PERCENT); //because the machine are not running at the beginning
+				changeLampMode(MACHINE_NOT_WORKING);
 
-						motor1_PID = taskSpawn("motor1", 210, 0, 1000, motor1);
-						motor2_PID = taskSpawn("motor2", 211, 0, 1000, motor2);
+				motor1_PID = taskSpawn("motor1", 210, 0, 1000, motor1);
+				motor2_PID = taskSpawn("motor2", 211, 0, 1000, motor2);
 
-						semMotor1 = semBCreate(SEM_Q_PRIORITY, SEM_FULL);
-						semMotor2 = semBCreate(SEM_Q_PRIORITY, SEM_FULL);
+				semMotor1 = semBCreate(SEM_Q_PRIORITY, SEM_FULL);
+				semMotor2 = semBCreate(SEM_Q_PRIORITY, SEM_FULL);
 
-						m1MsgQId = msgQCreate(10, sizeof(char), MSG_Q_FIFO);
-						m2MsgQId = msgQCreate(10, sizeof(char), MSG_Q_FIFO);
-						
-						break;
-					case 1:
-						//TODO: Let motor 1 advance one half-step. Turning off power immediately after.
-						break;
-					case 2:
-						//TODO: Let motor 2 advance one half-step. Turning off power immediately after.
-						break;
-				}
+				m1MsgQId = msgQCreate(10, sizeof(char), MSG_Q_FIFO);
+				m2MsgQId = msgQCreate(10, sizeof(char), MSG_Q_FIFO);
+
 				break;
+			case 1:
+				//TODO: Let motor 1 advance one half-step. Turning off power immediately after.
+
+				sysOutByte(0x181,M2_INHIB|M1_STEP);
+				taskDelay(25);
+				sysOutByte(0x181,M2_INHIB);
+				taskDelay(25);
+				sysOutByte(0x181,M2_INHIB|M1_INHIB);
+				taskDelay(50);
+				break;
+
+			case 2:
+				//TODO: Let motor 2 advance one half-step. Turning off power immediately after.
+
+				sysOutByte(0x181,M1_INHIB|M2_STEP);
+				taskDelay(25);
+				sysOutByte(0x181,M1_INHIB);
+				taskDelay(25);
+				sysOutByte(0x181,M1_INHIB|M2_INHIB);
+				taskDelay(50);
+				break;
+			}
+			break;
 			case ERR:
 				taskDelay(100);
 				//TODO: The motors should stop rotating immediately
@@ -107,38 +122,38 @@ void lab3() {
 				break;
 			default: 	//RUN
 				taskDelay(100);
-				
+
 				do { //reads from keyboard matrix until the user digit something on the keyboard
 					input = readKeyboard();
 					//taskDelay(10);
 				} while (input == -1 || (!(input>=0 && input<=6) && input!=10 && input!=11 && input!=15));
 
 				switch (input) {
-					case 0:
-						//TODO: Stop the motors at the next safe position
-						//TODO: Release the power to the engines		
-						changeLampMode(LAMP_OFF);	//turn of the lamp
-						changeFanMode(FAN_OFF);		//turn off the fan
-						stop();				//shutdown all processes
-						break;
-					case 15: //F: Emergency stop.
-						//TODO: Immediately stop all the stepping of the engines
-						changeFanMode(FAN_OFF);		//turns off the fan
-						changeLampMode(LAMP_OFF);	//turns off the lamp
-						changeRunningMode(ERR); //and enter error mode
-						break;
-					default:
-						if ((input>= 1 && input<= 3) || input == 10) {
-							msgQSend(m1MsgQId, (char)input, 1, WAIT_FOREVER, MSG_PRI_NORMAL); //send the input to motor1 message queue
-						}
-						if ((input>= 4 && input<= 6) || input == 11) {
-							msgQSend(m2MsgQId, (char)input, 1, WAIT_FOREVER, MSG_PRI_NORMAL); //send the input to motor2 message queue
-						}
-						break;
+				case 0:
+					//TODO: Stop the motors at the next safe position
+					//TODO: Release the power to the engines		
+					changeLampMode(LAMP_OFF);	//turn of the lamp
+					changeFanMode(FAN_OFF);		//turn off the fan
+					stop();				//shutdown all processes
+					break;
+				case 15: //F: Emergency stop.
+					//TODO: Immediately stop all the stepping of the engines
+					changeFanMode(FAN_OFF);		//turns off the fan
+					changeLampMode(LAMP_OFF);	//turns off the lamp
+					changeRunningMode(ERR); //and enter error mode
+					break;
+				default:
+					if ((input>= 1 && input<= 3) || input == 10) {
+						msgQSend(m1MsgQId, (char)input, 1, WAIT_FOREVER, MSG_PRI_NORMAL); //send the input to motor1 message queue
+					}
+					if ((input>= 4 && input<= 6) || input == 11) {
+						msgQSend(m2MsgQId, (char)input, 1, WAIT_FOREVER, MSG_PRI_NORMAL); //send the input to motor2 message queue
+					}
+					break;
 				}
 				break;
 		}
-		
+
 	}
 }
 
